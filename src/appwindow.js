31c1ada77eff04ab0b6ed23d2bf442f1768f6ba8
321Proteus <bgproteus@gmail.com>
2024-09-03 20:31:02 +0200

    Added window titlebars

--- src/appwindow.js
+++ src/appwindow.js
@@ -17,6 +17,26 @@ class AppWindow {
         windowElement.style.width = this.width + "px";
         windowElement.style.height = this.height + "px";
 
+        const titleBar = document.createElement("div");
+        titleBar.className = "title-bar";
+        windowElement.appendChild(titleBar);
+        titleBar.insertAdjacentHTML("beforeend", `<div class=window-title>${this.name}</div>`);
+
+        const titlebarButtons = document.createElement("div");
+        titlebarButtons.className = "titlebar-buttons";
+        titleBar.appendChild(titlebarButtons)
+
+        const fullscreenButton = document.createElement("img");
+        fullscreenButton.classList.add("btn-titlebar", "btn-full");
+        fullscreenButton.src = "../images/fullscreen.png";
+        titlebarButtons.appendChild(fullscreenButton);
+
+        const exitButton = document.createElement("img");
+        exitButton.classList.add("btn-titlebar", "btn-exit");
+        exitButton.src = "../images/exit.png";
+        exitButton.addEventListener("click", (e) => windowElement.style.display = "none");
+        titlebarButtons.appendChild(exitButton);
+
         const windowContent = document.createElement("div");
         windowContent.className = "window-content";
         if (html) windowContent.insertAdjacentElement("beforeend", html);
